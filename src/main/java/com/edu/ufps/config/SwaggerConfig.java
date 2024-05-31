bffe8b4e226f3bdda464315b439d0b490403773a
Samir Alexander Conde Pabón <90851389+Trolljerk@users.noreply.github.com>
2024-05-31 11:38:35 -0500

    Configuracion de Swagger

---
+++ src/main/java/com/edu/ufps/config/SwaggerConfig.java
@@ -0,0 +1,17 @@
+package com.edu.ufps.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.Components;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(new Info().title("Manga API").description("API para gestionar mangas y favoritos"));
+    }
+}
