9f0184aa6c274f62ff8e20489bd54c96de749b98
Samir Alexander Conde Pabón <90851389+Trolljerk@users.noreply.github.com>
2024-05-31 10:36:38 -0500

    Inicializacion de Spring

---
+++ src/test/java/com/edu/ufps/SegundoparcialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.ufps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SegundoparcialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
